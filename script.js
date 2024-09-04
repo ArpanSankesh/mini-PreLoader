f6cc1cf1f5e5f1129bc93b055a3408afc9b7733b
ArpanSankesh <arpansankesh830@gmail.com>
2024-09-04 16:36:39 +0530

    Gsap

--- script.js
+++ script.js
@@ -20,3 +20,32 @@ function startLoader(){
     updateCounter();
 }
 startLoader();
+
+gsap.to(".counter", 0.25,{
+    delay:3.5,
+    opacity:0,
+});
+
+gsap.to(".bar", 1.5,{
+    delay: 3.5,
+    height:0,
+    stagger:{
+        amount:0.5,
+    },
+    ease: "power4.inOut"
+});
+
+gsap.from("h1",1.5,{
+    delay:4,
+    y:700,
+    stagger:{
+        amount:0.5,
+    },
+    ease: "power4.inOut"
+});
+
+gsap.from(".hero", 2,{
+delay:4.5,
+y:400,
+ease: "power4.inOut"
+});
